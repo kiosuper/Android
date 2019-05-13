d594e3adc505bdbded3d2b3308c0638ff4cff294
AnduinBrian <36282595+AnduinBrian@users.noreply.github.com>
2019-05-13 08:24:23 +0700

    Update chính tả

--- Training/Android_Bai_Giua_Ky/GiuaKy/app/src/main/java/com/example/giuaky/XemActivity.java
+++ Training/Android_Bai_Giua_Ky/GiuaKy/app/src/main/java/com/example/giuaky/XemActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
